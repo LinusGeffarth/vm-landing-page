06924bedca85f74cbe87d7a178db4b2223d73000
Linus Geffarth <linus@geffarth.de>
2020-03-31 10:38:47 +0200

    no message

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, BrowserRouter } from "react-router-dom";
+import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
 
 import App from './App';
@@ -12,11 +12,9 @@ import './assets/scss/style.scss';
 const history = createBrowserHistory();
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <Router history={history}>
-      <App />
-    </Router>
-  </BrowserRouter>,
+  <Router history={history}>
+    <App />
+  </Router>,
   document.getElementById('root')
 );
 
